bcc9e8f066f3f0c2ccbc4afde6e0263d5ea1b59f
Chase yang <55267518+chase5ws@users.noreply.github.com>
2025-09-09 21:36:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function loginBot(token) {
           files: [attachment.url],   // 發送附件的URL
         });
       } else {
-        await postChannel.send("下方為你傳送的訊息\n" + message.content); //通知身分組
+        await postChannel.send(message.content); //通知身分組
       }
     }
   });
